0554d2518b162f0584c5608c0a74f4aa18abcf13
dibbodas12 <dibbodas228@gmail.com>
2023-04-05 15:02:02 +0600

    admin route added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -20,6 +20,7 @@ async function run(){
     try{
         const appointmentOptionCollection = client.db('perlourApplication').collection('appointmentOptions');
         const bookingsCollection = client.db('perlourApplication').collection('bookings');
+        const usersCollection = client.db('perlourApplication').collection('users');
 
         app.get('/appointmentOptions',async(req,res) => {
             const date = req.query.date;
@@ -36,10 +37,39 @@ async function run(){
             })
             res.send(options)
         })
+
+        app.get('/bookings',async(req,res) => {
+            const email = req.query.email;
+            const query = {email: email}
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings)
+        })
         app.post('/bookings',async(req,res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
             res.send(result)
+        })
+        app.post('/users',async(req,res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+        app.get('/users',async(req,res) => {
+            const query = {};
+            const result = await usersCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.put('/users/admin/:id',async(req,res) => {
+            const id = req.params.id;
+            const filter =  { _id : new ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set:{
+                    role : "admin"
+                }
+            }
+            const result = await usersCollection.updateOne(filter,updatedDoc,options);
+            res.send(result);
         })
 
     }
@@ -54,4 +84,4 @@ app.get('/',(req,res)=>{
     res.send('perlour app is running')
 })
 
-app.listen(port,()=>{console.log('perolour app runing on 5000');})
+app.listen(port,()=>{console.log(`perlour app runing on ${port}`)})
